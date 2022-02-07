21f8406e4c67182cefdf0722e9f60d9470969f45
julia-wrobel <julia.wrobel@cuanschutz.edu>
2022-02-07 08:39:33 -0700

    added zzz file for lazy loading

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+#' @importFrom ExperimentHub createHubAccessors
+#' @importFrom utils read.csv
+.onLoad <- function(libname, pkgname) {
+  fl <- system.file("extdata", "metadata.csv", package=pkgname)
+  titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+  createHubAccessors(pkgname, titles)
+}
