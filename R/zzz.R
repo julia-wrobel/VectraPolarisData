16056295e61914cb05bff71da0218d5796872640
Julia Wrobel <julia.wrobel@cuanschutz.edu>
2022-03-25 16:55:40 -0400

    updated data dictionaries

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 #' @importFrom ExperimentHub createHubAccessors
 #' @importFrom utils read.csv
+#' @import SpatialExperiment
 .onLoad <- function(libname, pkgname) {
   fl <- system.file("extdata", "metadata.csv", package=pkgname)
   titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
