35ba470cb25cac5ae9884cc649c8971af18b3909
Julia Wrobel <julia.wrobel@cuanschutz.edu>
2022-03-25 15:41:08 -0400

    updated vignette

--- R/VectraPolarisData.R
+++ R/VectraPolarisData.R
@@ -8,14 +8,13 @@
 #' @description
 #' Vectra 3 and Vectra Polaris multiplex imaging datasets,
 #' formatted into objects of class SpatialExperiment.
-#' Data include (DESCRIBE)
 #' Datasets may be used for testing of and as examples in packages,
 #' for tutorials and workflow demonstrations, or similar purposes.
 #'
 #' @details
 #' The following Vectra multiplex imaging
 #' datasets are currently available
-#' (see the package vignette for links to details):
+#' (see the package vignette for links to details and ddata dictionaries):
 #' \itemize{
 #' \item{LungCancerV3}
 #' \item{OvarianCancerVP}
@@ -25,21 +24,13 @@
 #' a \code{\linkS4class{SpatialExperiment}}
 #' with rows corresponding to samples (cells)
 #' and columns to features (markers).
-#' EXPLAIN OTHER FEATURES HERE
 #'
 #' @examples
-#' # EDIT THIS EXAMPLE
-#' # initialize hub instance
-#' eh <- VectraPolaris()
-#'
-#' # query for TENxVisium datasets
-#' (q <- query(eh, "VectraPolaris"))
-#'
 #' # retrieve dataset of interest
-#' pat <- "HumanCerebellum$"
-#' idx <- grep(pat, q$title)
-#' id <- q$ah_id[idx]
-#' (spe <- eh[[id]])
+#'
+#' library(VectraPolarisData)
+#' spe_lung <- HumanLungCancerV3()
+#' spe_ovarian <- HumanOvarianCancerVP()
 #'
 #'
 #' @author Julia Wrobel
